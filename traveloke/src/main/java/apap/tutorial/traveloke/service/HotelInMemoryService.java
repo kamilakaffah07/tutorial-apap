12d95246ad2010e16c51bc72426bb3bf1629800b
Kamila Kaffah <kaffahkamila@gmail.com>
2020-10-01 17:49:48 +0700

    add hotel, view hotel, view all hotel have done

---
+++ traveloke/src/main/java/apap/tutorial/traveloke/service/HotelInMemoryService.java
@@ -0,0 +1,39 @@
+package apap.tutorial.traveloke.service;
+
+import apap.tutorial.traveloke.model.HotelModel;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class HotelInMemoryService implements HotelService {
+
+    private List<HotelModel> listHotel;
+
+    //Constructor
+    public HotelInMemoryService(){
+        listHotel = new ArrayList<>();
+    }
+
+    @Override
+    public void addHotel(HotelModel hotel){
+        listHotel.add(hotel);
+    }
+
+    @Override
+    public List<HotelModel> getHotelList(){
+        return listHotel;
+    }
+
+    @Override
+    public HotelModel getHotelByIdHotel(String idhotel){
+        int indexHotel = 0;
+        for (int i = 0; i<listHotel.size(); i++){
+            if (listHotel.get(i).getIdHotel() == idhotel){
+                indexHotel = i;
+            }
+        }
+        return listHotel.get(indexHotel);
+    }
+}
